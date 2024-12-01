3c0208ff6e25c8e084b9fc64cd655edc2fcbd7f2
Aloxion <ansan21@student.sdu.dk>
2024-12-01 16:41:18 +0100

    sendgrid

---
+++ app/services/sendCard.service.ts
@@ -0,0 +1,23 @@
+/* eslint node/prefer-global/process: [error] */
+
+// Download the Node helper library from www.twilio.com/docs/libraries/node#installation
+// These identifiers are your accountSid and authToken from
+// https://www.twilio.com/console
+// To set up environmental variables, see http://twil.io/secure
+import * as sg from '@sendgrid/mail'
+
+export function sendCard(to: string, card: string, title: string): void {
+  const key = process.env.API_KEY || ''
+
+  sg.setApiKey(key)
+
+  const msg = {
+    to,
+    from: 'fra.julemanden.ho.ho.ho@gmail.com',
+    subject: title,
+    text: card,
+    html:
+    `<pre>${card}</pre>`,
+  }
+  sg.send(msg)
+}
